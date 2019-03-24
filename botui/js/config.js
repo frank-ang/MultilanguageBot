fa7aea2fdbe44ff91fe416c77c3f78e9b6331fb3
Frank Ang <frankang@amazon.com>
2019-03-24 23:01:22 +0800

    Fix typos

--- botui/js/config.js
+++ botui/js/config.js
@@ -1,9 +1,10 @@
 var config = {
     'region' : 'us-east-1',
-    'identity_pool_id' : 'us-east-1:4e771f11-9134-4702-960f-2e3b35252e23',
+    'identity_pool_id' : 'us-east-1:b1a31b96-c947-492e-8189-1e14eafa2de1',
     'user_pool_id': 'us-east-1_QZZnuKKLY',
     'user_pool_client_id': '3de3267rqnmg9hdd60btcurlni',
-    'orignal_api_url_with_cors_DELETE_ME': 'https://5q5qjlic60.execute-api.us-east-1.amazonaws.com/Prod/MultilanguageBot',
+    'orignal_api_url_with_cors': 'https://5q5qjlic60.execute-api.us-east-1.amazonaws.com/PROD/MultilanguageBot',
+    'should_delete_above_param': 'CLEANUP',
     'api_url': 'https://www.sandbox01.demolab.host/Prod/MultilanguageBot',
     'test_user_name': 'user01',
     'test_user_cred': 'Xoyxoz123.'
