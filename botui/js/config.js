9f659411ee3333ba5564e81c1bc637514e76d35b
Frank Ang <frankang@amazon.com>
2019-03-27 12:35:32 +0800

    fixing buildspec config substitution

--- botui/js/config.js
+++ botui/js/config.js
@@ -1,9 +1,9 @@
 var config = {
-    'region' : 'us-east-1',
-    'identity_pool_id' : 'us-east-1:b1a31b96-c947-492e-8189-1e14eafa2de1',
-    'user_pool_id': 'us-east-1_QZZnuKKLY',
-    'user_pool_client_id': '3de3267rqnmg9hdd60btcurlni',
-    'api_url': 'https://www.sandbox01.demolab.host/PROD/MultilanguageBot',
-    'test_user_name': 'user01',
-    'test_user_cred': 'Xoyxoz123.'
+    'region' : 'CHANGE_ME',
+    'identity_pool_id' : 'CHANGE_ME',
+    'user_pool_id': 'CHANGE_ME',
+    'user_pool_client_id': 'CHANGE_ME',
+    'api_url': 'CHANGE_ME',
+    'test_user_name': 'CHANGE_ME',
+    'test_user_cred': 'CHANGE_ME'
 }
