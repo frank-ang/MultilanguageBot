df96d23493243a5f538b44c2d181a42e1a52c5e3
Frank Ang <frankang@amazon.com>
2019-03-30 23:23:06 +0800

    Simple nested stack with only Cognito

--- syncTemplatesToS3.sh
+++ syncTemplatesToS3.sh
@@ -2,4 +2,8 @@
 # Syncs Cloudformation templates to the S3 bucket to permit top-level template to reference nested stacks 
 
 
-# aws s3 sync ...
+aws s3 cp ./MultilanguageApi.yaml s3://multilanguage-bot/ --acl public-read 
+aws s3 cp ./MultilanguageBotStack.yaml s3://multilanguage-bot/ --acl public-read 
+aws s3 cp ./cognito/cognito-cfn.yaml s3://multilanguage-bot/cognito/ --acl public-read 
+aws s3 cp ./edge/edge-site.yaml s3://multilanguage-bot/edge/ --acl public-read 
+aws s3 cp ./pipeline/pipeline.yaml s3://multilanguage-bot/pipeline/ --acl public-read 
