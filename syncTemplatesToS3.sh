83bb67c1a774edcf472f9dbd1c0e2df12f01b4cd
Frank Ang <frankang@amazon.com>
2019-04-01 16:56:53 +0800

    3-nested stack creation success. Moved nested templates to cloudformation subfolder.

--- syncTemplatesToS3.sh
+++ syncTemplatesToS3.sh
@@ -4,6 +4,6 @@
 
 aws s3 cp ./MultilanguageApi.yaml s3://multilanguage-bot/ --acl public-read 
 aws s3 cp ./MultilanguageBotStack.yaml s3://multilanguage-bot/ --acl public-read 
-aws s3 cp ./cognito/cognito-cfn.yaml s3://multilanguage-bot/cognito/ --acl public-read 
-aws s3 cp ./edge/edge-site.yaml s3://multilanguage-bot/edge/ --acl public-read 
-aws s3 cp ./pipeline/pipeline.yaml s3://multilanguage-bot/pipeline/ --acl public-read 
+aws s3 cp ./cloudformation/cognito.yaml s3://multilanguage-bot/cloudformation/ --acl public-read 
+aws s3 cp ./cloudformation/edge-site.yaml s3://multilanguage-bot/cloudformation/ --acl public-read 
+aws s3 cp ./cloudformation/pipeline.yaml s3://multilanguage-bot/cloudformation/ --acl public-read 
