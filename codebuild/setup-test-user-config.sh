6695e20a0239ea24bdfed9f5da2cb759c2776815
Frank Ang <frankang@amazon.com>
2019-04-08 23:09:25 +0800

    fix missing variables in codebuild script

--- codebuild/setup-test-user-config.sh
+++ codebuild/setup-test-user-config.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+# 
+# Setup Cognito test user, with cred set in SecretsManager 
+#
 set -e
 echo "Creating cognito user..."
 
-echo "DEBUG printing variables."
+echo "DEBUG printing variables..."
+echo "IDENTITY_POOL_ID=$IDENTITY_POOL_ID"
 echo "USER_POOL_ID=$USER_POOL_ID"
 echo "USER_POOL_CLIENT_ID=$USER_POOL_CLIENT_ID"
+echo "API_URL=$API_URL"
 echo "TEST_USER_SECRET_ID=$TEST_USER_SECRET_ID"
 echo "TEST_USER_NAME=$TEST_USER_NAME"
 
 echo "Checking for required environment variables..."
+: "${IDENTITY_POOL_ID?IDENTITY_POOL_ID needs to be set}"
 : "${USER_POOL_ID?USER_POOL_ID needs to be set}"
 : "${USER_POOL_CLIENT_ID?USER_POOL_CLIENT_ID needs to be set}"
+: "${API_URL?API_URL needs to be set}"
 : "${TEST_USER_SECRET_ID?TEST_USER_SECRET_ID needs to be set}"
 : "${TEST_USER_NAME?TEST_USER_NAME needs to be set}"
+
 
 # Sanity check that the config file exists.
 if [ ! -f $JS_CONFIG_FILE ]; then
@@ -42,12 +50,7 @@ then
 	echo "Confirming user by responding to auth challenge..."
 	aws cognito-idp admin-respond-to-auth-challenge --user-pool-id $USER_POOL_ID --client-id $USER_POOL_CLIENT_ID --challenge-name NEW_PASSWORD_REQUIRED --challenge-responses NEW_PASSWORD=$TEST_USER_CRED,USERNAME=$TEST_USER_NAME,userAttributes.name=$TEST_USER_NAME --session $SESSION_KEY
 	
-	echo "Verifying authentication as the test user to retrieve OAUTH_ID_TOKEN JWT Bearer token" 
-	OAUTH_ID_TOKEN=`aws cognito-idp initiate-auth --client-id $USER_POOL_CLIENT_ID --auth-flow USER_PASSWORD_AUTH --auth-parameters USERNAME=$TEST_USER_NAME,PASSWORD=$DESIRED_PASSWORD | jq -r ".AuthenticationResult.IdToken"`
-	
-	echo "#DEBUG# OAUTH_ID_TOKEN=$OAUTH_ID_TOKEN" 
-
-	# Write Secrets.
+	# Write cred to SecretsManager.
 	SECRET_STRING='[{"username":"' 
 	SECRET_STRING+=$TEST_USER_NAME
 	SECRET_STRING+='"},{"password":"'
@@ -61,13 +64,17 @@ then
 
 else
 
-    # Read the creds from Secrets Manager
+    # Read the cred from Secrets Manager
 	echo "Cognito test user=$TEST_USER_NAME already exists. Retrieving test creds."
-	TEST_USER_CRED=`aws secretsmanager get-secret-value --secret-id $TEST_USER_SECRET_ID  | ./jq -r '.SecretString' | ./jq  -r '.[].password | select(length>0)'`
+	TEST_USER_CRED=`aws secretsmanager get-secret-value --secret-id $TEST_USER_SECRET_ID`
+	TEST_USER_CRED=`echo $TEST_USER_CRED | ./jq -r '.SecretString' | ./jq  -r '.[].password | select(length>0)'`
 
 fi
 
 echo "debug: Test user cred=$TEST_USER_CRED" 
+
+echo "Verifying authentication as the test user to retrieve OAUTH_ID_TOKEN JWT Bearer token" 
+OAUTH_ID_TOKEN=`aws cognito-idp initiate-auth --client-id $USER_POOL_CLIENT_ID --auth-flow USER_PASSWORD_AUTH --auth-parameters USERNAME=$TEST_USER_NAME,PASSWORD=$TEST_USER_CRED | jq -r ".AuthenticationResult.IdToken"`
 
 echo "Setting parameters into the website config.js file, at: $JS_CONFIG_FILE"
 
