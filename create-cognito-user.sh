df54c8f1131466687ef008c802accdf98df2d276
Frank Ang <frankang@amazon.com>
2019-04-08 14:30:33 +0800

    user exists check

--- create-cognito-user.sh
+++ create-cognito-user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ev
+set -e
 echo "Creating cognito user..."
 
 echo "DEBUG print variables"
@@ -15,7 +15,6 @@ echo "Checking for required environment variables..."
 : "${USER_POOL_CLIENT_ID?USER_POOL_CLIENT_ID needs to be set}"
 : "${TEST_USER_SECRET_ID?TEST_USER_SECRET_ID needs to be set}"
 : "${TEST_USER_NAME?TEST_USER_NAME needs to be set}"
-: "${TEST_USER_NAME?TEST_USER_NAME needs to be set}"
 : "${TEST_USER_CRED?TEST_USER_CRED needs to be set}"
 
 TEST_USER_TEMP_CRED=Temp123.
@@ -26,10 +25,13 @@ TEST_USER_TEMP_CRED=Temp123.
 # TEST_USER_CRED=`date | md5sum | head -c${1:-10}`
 
 # Check if the test user exists.
-aws cognito-idp admin-get-user --user-pool-id $USER_POOL_ID --username $TEST_USER_NAME
-if [ $? -ne 0 ]; then
+USER_EXISTS_RC=-1
+aws cognito-idp admin-get-user --user-pool-id $USER_POOL_ID --username $TEST_USER_NAME && USER_EXISTS_RC=$? || USER_EXISTS_RC=$?
+echo "USER_EXISTS_RC=$USER_EXISTS_RC"
 
-    # 1. setup test user.
+if [ #USER_EXISTS_RC -ne 0 ]; then
+
+    # 1. Create and Confirm test user.
     echo "Creating user = $TEST_USER_NAME" | jq -r '.UserStatus'
     aws cognito-idp admin-create-user --user-pool-id $USER_POOL_ID --username $TEST_USER_NAME --temporary-password $TEST_USER_TEMP_CRED
 	echo "Getting session key administratively as test user"
@@ -37,7 +39,7 @@ if [ $? -ne 0 ]; then
 	echo "Confirming user by responding to auth challenge..."
 	aws cognito-idp admin-respond-to-auth-challenge --user-pool-id $USER_POOL_ID --client-id $USER_POOL_CLIENT_ID --challenge-name NEW_PASSWORD_REQUIRED --challenge-responses NEW_PASSWORD=$TEST_USER_CRED,USERNAME=$TEST_USER_NAME,userAttributes.name=$TEST_USER_NAME --session $SESSION_KEY
 	echo "Verifying authentication as the test user to retrieve OAUTH_ID_TOKEN JWT Bearer token" 
-	OAUTH_ID_TOKEN=`aws cognito-idp initiate-auth --client-id $USER_POOL_CLIENT_ID --auth-flow USER_PASSWORD_AUTH --auth-parameters USERNAME=$USERNAME,PASSWORD=$DESIRED_PASSWORD | jq -r ".AuthenticationResult.IdToken"`
+	OAUTH_ID_TOKEN=`aws cognito-idp initiate-auth --client-id $USER_POOL_CLIENT_ID --auth-flow USER_PASSWORD_AUTH --auth-parameters USERNAME=$TEST_USER_NAME,PASSWORD=$DESIRED_PASSWORD | jq -r ".AuthenticationResult.IdToken"`
 	echo "#debug# OAUTH_ID_TOKEN=$OAUTH_ID_TOKEN"
 
 	# 1. Write Secrets.
@@ -51,7 +53,8 @@ if [ $? -ne 0 ]; then
 	aws secretsmanager create-secret --name $TEST_USER_SECRET_ID --description "Test user cred" --secret-string $SECRET_STRING
 
 	echo "completed test user setup"
-
+else
+	echo "Cognito user=$TEST_USER_NAME already exists. Skipping user creation."
 fi
 
 echo "completed"
