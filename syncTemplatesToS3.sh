7ef5a253cd44ba0b52d810349b0943145a7d1b41
Frank Ang <frankang@amazon.com>
2019-04-09 09:24:42 +0800

    Rename CFN template files. Fix pipeline deploy IAM capabilities.

--- syncTemplatesToS3.sh
+++ syncTemplatesToS3.sh
@@ -1,9 +1,5 @@
 # syncTemplatesToS3.sh
 # Syncs Cloudformation templates to the S3 bucket to permit top-level template to reference nested stacks 
 
-
-aws s3 cp ./MultilanguageApi.yaml s3://multilanguage-bot/ --acl public-read 
-aws s3 cp ./MultilanguageBotStack.yaml s3://multilanguage-bot/ --acl public-read 
-aws s3 cp ./cloudformation/cognito.yaml s3://multilanguage-bot/cloudformation/ --acl public-read 
-aws s3 cp ./cloudformation/edge-site.yaml s3://multilanguage-bot/cloudformation/ --acl public-read 
-aws s3 cp ./cloudformation/pipeline.yaml s3://multilanguage-bot/cloudformation/ --acl public-read 
+aws s3 cp ./multilanguage-bot-stack.yaml s3://multilanguage-bot/ --acl public-read 
+aws s3 sync ./cloudformation/ s3://multilanguage-bot/cloudformation/ --acl public-read 
